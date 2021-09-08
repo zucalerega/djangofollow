26ffeebea807b405ff12235432280178f2a9423e
zucalerega <85199881+zucalerega@users.noreply.github.com>
2021-09-07 21:03:11 -0500

    Update settings.py

--- djangofollow/djangofollow/settings.py
+++ djangofollow/djangofollow/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i(ss43su15!*5#@d^#e!0&0u1r&9hcmp$px-4m7@6m8(3ua$z7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
